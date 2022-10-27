abdcc2f51fef886fb6693364083c191b07dd8d0b
Kota Suzuki <s21c1064cl@s.chibakoudai.jp>
2022-10-27 12:59:31 +0900

    test script

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv 
 # SPDX-FileCopyrightText: 2022 Kota Suzuki s21c1064cl@s.chibakoudai.jp
 # SPDX-License Identifier: BSD-3-Clause
 
@@ -12,6 +12,16 @@ res=0
 ###I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+###STRANGE INPUT###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng $[LINENO]
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]       || ng ${LINENO}
+[ "${out}" = "" ]  || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo ok
 exit $res
